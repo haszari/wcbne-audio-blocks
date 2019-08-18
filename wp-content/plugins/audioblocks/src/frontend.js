38ac398d331ef6daba0d6a0fd14bf49a40254a72
haszari <haszari@cartoonbeats.com>
2019-08-18 15:31:24 +1200

    refactor: loadSample into lib module

--- wp-content/plugins/audioblocks/src/frontend.js
+++ wp-content/plugins/audioblocks/src/frontend.js
@@ -1,19 +1,8 @@
 
+import loadSample from './lib/preload-audio-sample';
 
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioContext = new AudioContext();
-
-function loadSample(url, audioContext, callback) {
-	var request = new XMLHttpRequest();
-	request.open('GET', url);
-	request.responseType = 'arraybuffer';
-	request.onload = function () {
-		console.log('sample loaded, decoding');
-		audioContext.decodeAudioData(request.response, callback);
-	};
-	console.log('loading sample');
-	request.send();
-}
 
 function getSecondsPerBeat( tempoBpm ) {
 	return 60.0 / tempoBpm;
---
+++ wp-content/plugins/audioblocks/src/lib/preload-audio-sample.js
@@ -0,0 +1,14 @@
+
+function loadSample(url, audioContext, callback) {
+	var request = new XMLHttpRequest();
+	request.open('GET', url);
+	request.responseType = 'arraybuffer';
+	request.onload = function () {
+		console.log('sample loaded, decoding');
+		audioContext.decodeAudioData(request.response, callback);
+	};
+	console.log('loading sample');
+	request.send();
+}
+
+export default loadSample;
